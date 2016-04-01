24f459cc4d40b098090249d760785a5380019940
Nick <dufour@neon-lab.com>
2016-03-31 20:35:42 -0700

    stuffs

---
+++ train_aquila.py
@@ -0,0 +1,9 @@
+"""
+Actually accomplishes the training of Aquila.
+"""
+
+from training.input import InputManager
+from net.slim.aquila_model import aquila
+from net.slim.losses import ranknet_loss
+from net.slim.losses import accuracy
+
