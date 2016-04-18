bd01c25767103b1e4140f7a6f21f9cc0bcf0e923
David Lea <lea@neon-lab.com>
2016-04-18 15:07:54 -0700

    trial run of score images

--- score_images.py
+++ score_images.py
@@ -19,10 +19,31 @@ AQUILA_SNAP = '/data/aquila_snaps_lowreg/model.ckpt-250000'
 images = glob(os.path.join(IMAGE_DIR, '*.jpg'))
 
 
-inputs = tf.placeholder(tf.float32, shape=[22, 299, 299, 3])
+fn_phd = tf.placeholder(tf.string, shape=[])
+raw_im = tf.read_file(fn_phd)
+image = tf.image.decode_jpeg(raw_im, channels=3)
+image = tf.expand_dims(image, 0)
+image = tf.image.resize_bilinear(image, [299, 299],
+                                 align_corners=False)
+
 # instantiate aquila
-logits, abst_feats = aquila.abst_feats(
-	inputs, abs_feats, for_training=False,
-    restore_logits=True, scope=scope,
-    regularization_strength=WEIGHT_DECAY,
-    dropout_keep_prob=DROPOUT_KEEP_PROB)
+with tf.device('/gpu:0'):
+	logits, abst_feats = aquila.abst_feats(
+		image, abs_feats, for_training=False,
+	    restore_logits=True, scope=None,
+	    regularization_strength=WEIGHT_DECAY,
+	    dropout_keep_prob=DROPOUT_KEEP_PROB)
+
+init = tf.initialize_all_variables()
+sess = tf.InteractiveSession()
+sess.run(init)
+
+variables_to_restore = tf.get_collection(
+                slim.variables.VARIABLES_TO_RESTORE)
+restorer = tf.train.Saver(variables_to_restore)
+
+# restore it!
+restorer.restore(sess, AQUILA_SNAP)
+
+_logits, _abst_feats = sess.run([logits, abst_feats], 
+								feed_dict={fn_phd: images[0]})
