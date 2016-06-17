bugfix in loss computation

aquila_train.py
@@ -259,23 +259,24 @@ def train(inp_mgr, ex_per_epoch):
           (datetime.now(), max_steps))
     for step in xrange(1, max_steps):
         start_time = time.time()
-        _, loss_value = sess.run([train_op, loss])
+        _, loss_value, lr_float = sess.run([train_op, loss, lr])
         duration = time.time() - start_time
         if inp_mgr.should_stop():
             break
         if np.isnan(loss_value):
             print('Model is diverging (omg!) dumping data')
-            summary_str = sess.run(summary_op)
-            summary_writer.add_summary(summary_str, step)
-            checkpoint_path = os.path.join(train_dir, 'model.ckpt')
-            saver.save(sess, checkpoint_path, global_step=step)
-            raise Exception('Model diverged with loss = NaN on epoch %i' % step)
+            # summary_str = sess.run(summary_op)
+            # summary_writer.add_summary(summary_str, step)
+            # checkpoint_path = os.path.join(train_dir, 'model.ckpt')
+            # saver.save(sess, checkpoint_path, global_step=step)
+            raise Exception('Model diverged with loss = NaN on step %i' % step)
         if step % 1 == 0:
             examples_per_sec = BATCH_SIZE / float(duration)
             format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; '
-                          '%.3f sec/batch)')
+                          '%.3f sec/batch) (lr is %g')
             print(format_str % (datetime.now(), step, loss_value,
-                                                    examples_per_sec, duration))
+                                                    examples_per_sec,
+                                duration, lr_float))
 
         if step % 200 == 0:
             summary_str = sess.run(summary_op)

testing/test_input_v2.py
@@ -18,19 +18,19 @@ with open('/tmp/aquila_test_data/combined', 'r') as f:
         gtd[(a, b)] = dat[:DEMOGRAPHIC_GROUPS]
         gtd[(b, a)] = dat[DEMOGRAPHIC_GROUPS:]
 
-tf_queue = tf.FIFOQueue(BATCH_SIZE, [tf.float32, tf.uint8, tf.float32,
-                                     tf.string],
-                      shapes=[[299, 299, 3],
-                              [BATCH_SIZE, DEMOGRAPHIC_GROUPS],
-                              [BATCH_SIZE, DEMOGRAPHIC_GROUPS],
-                              []])
+tf_queue = tf.FIFOQueue(BATCH_SIZE * num_gpus * 2,
+                        [tf.float32, tf.float32, tf.float32, tf.string],
+                        shapes=[[299, 299, 3],
+                                [BATCH_SIZE, DEMOGRAPHIC_GROUPS],
+                                [BATCH_SIZE, DEMOGRAPHIC_GROUPS],
+                                []])
 image_phds = [tf.placeholder(tf.string, shape=[]) for _ in range(BATCH_SIZE)]
-label_phds = [tf.placeholder(tf.uint8, shape=[BATCH_SIZE, DEMOGRAPHIC_GROUPS])
+label_phds = [tf.placeholder(tf.float32, shape=[BATCH_SIZE, DEMOGRAPHIC_GROUPS])
               for _ in range(BATCH_SIZE)]
 conf_phds = [tf.placeholder(tf.float32, shape=[BATCH_SIZE, DEMOGRAPHIC_GROUPS])
              for _ in range(BATCH_SIZE)]
-
 enqueue_op = get_enqueue_op(image_phds, label_phds, conf_phds, tf_queue)
+
 im = InputManager(image_phds, label_phds, conf_phds, tf_queue,
                   enqueue_op, num_epochs=2, num_qworkers=1)
 