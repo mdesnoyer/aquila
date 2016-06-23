19196963dd45b0d7928915f83178f3e773954517
Nick <dufour@neon-lab.com>
2016-06-23 15:41:29 -0700

    modification of the training data creation to bring it up to snuff and unify it with the version that i put into the mturk task

--- testing/create_training_data.py
+++ testing/create_training_data.py
@@ -1,13 +1,140 @@
-'''
-This is a test script for the more efficient version of training for aquila v2
+# assemble the training set.
+# 0 = <= 29
+# 1 = 30 - 39
+# 2 = 40 - 49
+# 3 = 50+
+# 4 = Unk
 
-The way it works is:
-    Read in the data, then randomly select an image, adding pairs as you go to
-    a set of images. Then, assemble all the data together.
-'''
+# male = 0
+# female = 5
 
-import numpy as np
+# 0 = male / <= 29
+# 1 = male / 30 - 39
+# 2 = male / 40 - 49
+# 3 = male / 50+
+# 4 = female / <= 29
+# 5 = female / 30 - 39
+# 6 = female / 40 - 49
+# 7 = female / 50+
+# 8 = Unk / Unk
+
+# bins = Counter()
+# for i in f:
+#     a, b, c, d, e = i.split(',')
+#     bins[d] += 1
+
+# for i in f:
+#     a, b, c, d, e = i.split(',')
+#     bins[c] += 1
+
+# i.e.,
+# (a, b) = < # of times a beat b by demo> < # of times b beat a by demo >
+
 from collections import defaultdict as ddict
+import locale
+import sys
+
+try:
+    # for linux
+    locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+except:
+    # for mac
+    locale.setlocale(locale.LC_ALL, 'en_US')
+
+test_size = 1000  # the number of EDGES in the testing set, such that at
+# least one image in each pair (i.e., edge) is in the testing set. What we
+# could do is greedily add images, by selecting the images with the minimum
+# number of edges.
+
+# first step is to determine all the available images that we've got
+all_ims = dict()
+with open('/data/aquila_data/avail_images/imagelist', 'r') as f:
+    for line in f:
+        img = line.strip()
+        all_ims[img.split('.')[0]] = img
+
+age_bin_correspondence = {  # age bin 2 age interval
+    0: (0, 19),
+    1: (20, 29),
+    2: (30, 39),
+    3: (40, 49),
+    4: (50, float("inf")),
+    5: 'unknown'
+}
+
+gend_bin_correspondence = {  # gender bin to gender
+    0: 'male',
+    1: 'female',
+    2: 'unknown'
+}
+
+age_gend_abs_bin = {  # age bin, gender bin to absolute bin
+    (0, 0): 0,
+    (1, 0): 1,
+    (2, 0): 2,
+    (3, 0): 3,
+    (4, 0): 4,
+    (0, 1): 5,
+    (1, 1): 6,
+    (2, 1): 7,
+    (3, 1): 8,
+    (4, 1): 9,
+    # 10 is unknown
+}
+
+old_age_bins = {
+    '18-29': 0,
+    '30-39': 2,
+    '40-49': 3,
+    '20-29': 1,
+    '50-59': 4,
+    '': 5,
+    '60-69': 4,
+    'None': 5,
+    '0-19': 0,
+    '70+': 4}
+
+new_age_bins = {(0, 19): 0,
+            (20, 29): 1,
+            (30, 39): 2,
+            (40, 49): 3,
+            (50, 59): 4,
+            (60, 69): 4,
+            (70, 100): 4,
+            (-7977, -7977): 1}
+
+
+def new_age_to_bin(nage):
+    """converts a new age to a bin"""
+    if not nage:
+        return 6
+    else:
+        k = int(nage)
+        for a, b in new_age_bins:
+            if a <= k and k <= b:
+                return new_age_bins[(a, b)]
+
+
+def old_age_to_bin(oage):
+    """converts an old age to a bin"""
+    return old_age_bins[oage]
+
+
+def gender_to_bin(gend):
+    """ works for both """
+    if gend == 'male':
+        return 0
+    elif gend == 'female':
+        return 1
+    else:
+        return 2
+
+
+def age_gend_bin_to_comb(age_bin, gend_bin):
+    """ combines an age bin and a gender bin """
+    return age_gend_abs_bin.get((age_bin, gend_bin), 10)
+
+
 import locale
 
 try:
@@ -17,120 +144,144 @@ except:
     # for mac
     locale.setlocale(locale.LC_ALL, 'en_US')
 
-# args
-# TODO: Ensure that this are set by the conf file.
-DATA_LOC = '/Users/ndufour/Desktop/mturk_data/combined'
-BATCH_SIZE = 22
-DEMOGRAPHIC_GROUPS = 9
 
-assert not BATCH_SIZE % 2, 'BATCH_SIZE must be an even number'
+data = ddict(lambda: [0] * 22)
+data_test = ddict(lambda: [0] * 22)
 
-pairs = ddict(lambda: set())
-labels = dict()
+tot = 0
+bad = 0
+bad_ims_v1 = set()
+bad_ims_v2 = set()
+good_im_cnt = 0
 
-# build the pairs
-with open(DATA_LOC, 'r') as f:
-    for n, line in enumerate(f):
-        cur_data = line.strip().split(',')
-        img_a = cur_data[0]
-        img_b = cur_data[1]
-        outcomes = [int(x) for x in cur_data[2:]]
-        pairs[img_a].add(img_b)
-        labels[(img_a, img_b)] = np.array(outcomes).astype(int)
-        if not n % 1000:
-            print 'Total read: %s' % locale.format("%d", n, grouping=True)
-print 'Total read: %s' % locale.format("%d", n, grouping=True)
-total_pairs = n
-
-# there's a potential problem, in that since we're not adding an image to a
-# batch more than once, we might encounter an image that was paired 20 times,
-# and so the total number of images in the batch is 21. If this is the case,
-# perhaps we should maintain two lists, a pending list and a current list.
-# actually, no, we'll just add uneven numbers of pairs.
-def yield_sets(i, pairs, pending_batches=[]):
-    """
-    Given pending batches, and a pair group (i -> pairs), this will construct
-    and sequentially yield appropriate batches. At most two incomplete batches
-    are generated.
-
-    NOTES:
-        WARNING:
-            Modifies pending batches in-place.
-
-    Args:
-        i: The first pair item.
-        pairs: A list of items paired with i
-        pending_batches: Incomplete batches.
-
-    Returns: An iterator over the assembled batches.
-    """
-
-    def get_next_batch():
-        if len(pending_batches):
-            return pending_batches.pop()
-        return set()
-
-    batch = get_next_batch()
-    while pairs:
-        if len(batch) == BATCH_SIZE:
-            yield batch
-            batch = get_next_batch()
+with open('/data/aquila_data/mturk_task_v1/data', 'r') as f:
+    for line in f:
+        win, lose, gender, age_b, wid = line.strip().split(',')
+        win = win.split('.')[0]
+        lose = lose.split('.')[0]
+        if win not in all_ims:
+            bad_ims_v1.add(win)
             continue
-        if len(batch | set(pairs) | set((i,))) == BATCH_SIZE - 1:
-            # then it will produce aliasing.
-            batch.add(i)
-            while len(batch) < BATCH_SIZE - 2:
-                batch.add(pairs.pop())
-            yield batch
-            batch = get_next_batch()
+        if lose not in all_ims:
+            bad_ims_v1.add(lose)
             continue
-        batch.add(i)
-        batch.add(pairs.pop())
-        if len(batch) == BATCH_SIZE:
-            yield batch
-            batch = get_next_batch()
-    if batch:
-        yield batch
-    while pending_batches:
-        yield pending_batches.pop()
+        good_im_cnt += 2
+        if not win or not lose:
+            # some records have only a win or a lose, not both. arg.
+            bad += 1
+            continue
+        if win < lose:
+            key = (win, lose)
+            base_v = 0
+        elif win > lose:
+            key = (lose, win)
+            base_v = 11  # offset the bin by 8 because it's a loss
+
+        age_b = old_age_to_bin(age_b)
+        gend_b = gender_to_bin(gender)
+        demo_idx = age_gend_bin_to_comb(age_b, gend_b)
+
+        data[key][demo_idx + base_v] += 1
+        tot += 1
+        if not tot % 10000:
+            v2 = locale.format("%d", tot, grouping=True)
+            bs = locale.format("%d", bad + len(bad_ims_v1), grouping=True)
+            print '[v1] %s records collected (%s bad)' % (v2, bs)
+tot_old = len(data)
+
+with open('/data/aquila_data/mturk_task_v2/data', 'r') as f:
+    for line in f:
+        win, lose, gender, age, wid, tid = line.strip().split(',')
+        if win.split('.')[0] not in all_ims:
+            bad_ims_v2.add(win)
+            continue
+        if lose.split('.')[0] not in all_ims:
+            bad_ims_v2.add(lose)
+            continue
+        good_im_cnt += 2
+        if not win or not lose:
+            bad += 1
+            continue
+        if win < lose:
+            key = (win, lose)
+            base_v = 0
+        elif win > lose:
+            key = (lose, win)
+            base_v = 11  # offset the bin by 8 because it's a loss
+        age_b = new_age_to_bin(age)
+        gend_b = gender_to_bin(gender)
+        demo_idx = age_gend_bin_to_comb(age_b, gend_b)
+        data[key][demo_idx + base_v] += 1
+        tot += 1
+        if not tot % 10000:
+            v2 = locale.format("%d", tot, grouping=True)
+            bs = locale.format("%d", bad + len(bad_ims_v2), grouping=True)
+            print '[v2] %s records collected (%s bad)' % (v2, bs)
 
 
-def batch_gen(pairs):
-    cur_batch = set()
-    pending_batches = []
-    while True:
-        np.random.shuffle(pairs)
-        for i in pairs:
-            pair_items = pairs[i]
-            constructed_batches = yield_sets(i, pair_items, pending_batches)
-            pending_batches = []
-            for batch in constructed_batches:
-                if len(batch) != BATCH_SIZE:
-                    pending_batches.append(batch)
-                else:
-                    yield batch
+with open('/data/aquila_data/mturk_task_v2/data_test', 'r') as f:
+    for line in f:
+        win, lose, gender, age, wid, tid = line.strip().split(',')
+        if win.split('.')[0] not in all_ims:
+            bad_ims_v2.add(win)
+            continue
+        if lose.split('.')[0] not in all_ims:
+            bad_ims_v2.add(lose)
+            continue
+        good_im_cnt += 2
+        if not win or not lose:
+            bad += 1
+            continue
+        if win < lose:
+            key = (win, lose)
+            base_v = 0
+        elif win > lose:
+            key = (lose, win)
+            base_v = 11  # offset the bin by 8 because it's a loss
+        age_b = new_age_to_bin(age)
+        gend_b = gender_to_bin(gender)
+        demo_idx = age_gend_bin_to_comb(age_b, gend_b)
+        data_test[key][demo_idx + base_v] += 1
+        tot += 1
+        if not tot % 10000:
+            v2 = locale.format("%d", tot, grouping=True)
+            bs = locale.format("%d", bad + len(bad_ims_v2), grouping=True)
+            print '[v2 test] %s records collected (%s bad)' % (v2, bs)
 
 
-def gen_labels(batch, labels):
-    '''
-    Generates a label, given a batch and the labels dictionary.
+tot_new = len(data) - tot_old
 
-    Args:
-        batch: The current batch, as a set.
-        labels: The labels dictionary.
 
-    Returns: The batch, and a win matrix, of size batch x batch x demographic
-    groups
-    '''
-    win_matrix = np.zeros((BATCH_SIZE, BATCH_SIZE, DEMOGRAPHIC_GROUPS))
-    lb = list(batch)
-    for m, i in enumerate(lb):
-        for n, j in enumerate(lb):
-            if (i, j) in labels:
-                win_matrix[m, n, :] = labels[(i,j)][:DEMOGRAPHIC_GROUPS]
-                win_matrix[n, m, :] = labels[(i,j)][DEMOGRAPHIC_GROUPS:]
-    return lb, win_matrix
+with open('/data/aquila_data/combined', 'w') as f:
+    for n, k in enumerate(sorted(data.keys())):
+        if not n % 10000:
+            v2 = locale.format("%d", n, grouping=True)
+            print '%s records written' % v2
+        a, b = k
+        winstr = ','.join([str(x) for x in data[k]])
+        f.write('%s,%s,%s\n' % (all_ims[a], all_ims[b], winstr))
 
-bg = batch_gen(pairs)
-next_batch = bg.next()
-batch, win_matrix = gen_labels(next_batch, labels)
+
+with open('/data/aquila_data/combined_testing', 'w') as f:
+    for n, k in enumerate(sorted(data_test.keys())):
+        if not n % 10000:
+            v2 = locale.format("%d", n, grouping=True)
+            print '%s records written' % v2
+        a, b = k
+        winstr = ','.join([str(x) for x in data_test[k]])
+        f.write('%s,%s,%s\n' % (all_ims[a], all_ims[b], winstr))
+
+
+with open('/data/aquila_data/bad_v1', 'w') as f:
+    f.write('\n'.join(bad_ims_v1))
+
+with open('/data/aquila_data/bad_v2', 'w') as f:
+    f.write('\n'.join(bad_ims_v2))
+
+v1o = locale.format("%d", tot_old, grouping=True)
+v2o = locale.format("%d", tot_new, grouping=True)
+
+v2 = locale.format("%d", n, grouping=True)
+print '%s obtained from v1, %s obtained from v2' % (v1o, v2o)
+print '%s records written' % v2
+print 'All files written'
