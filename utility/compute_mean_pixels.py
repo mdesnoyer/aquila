ecc7655d91fada89014e53416d14865aaa561717
Nick <dufour@neon-lab.com>
2016-06-23 11:59:35 -0700

    bugfix

--- utility/compute_mean_pixels.py
+++ utility/compute_mean_pixels.py
@@ -26,23 +26,28 @@ print 'Computing mean values'
 mean_vals = [0, 0, 0]
 count = 0
 not_avail = set()
+fail = set()
+
 for im in unique_ims:
-    imfn = os.path.join(im_src, im)
-    if os.path.exists(imfn):
-        x = np.array(Image.open(imfn).convert('RGB'))
-        r, g, b = np.mean(x, (0, 1))
-        mean_vals[0] += r
-        mean_vals[1] += g
-        mean_vals[2] += b
-        count += 1
-        if not count % 1000:
-            mr = mean_vals[0] / count
-            mg = mean_vals[1] / count
-            mb = mean_vals[2] / count
-            print '%i: r:%.3f g:%.3f b:%.3f (unav: %i)' % (count, mr, mg, mb,
-                                                           len(not_avail))
-    else:
-        not_avail.add(im)
+    try:
+        imfn = os.path.join(im_src, im)
+        if os.path.exists(imfn):
+            x = np.array(Image.open(imfn).convert('RGB'))
+            r, g, b = np.mean(x, (0, 1))
+            mean_vals[0] += r
+            mean_vals[1] += g
+            mean_vals[2] += b
+            count += 1
+            if not count % 1000:
+                mr = mean_vals[0] / count
+                mg = mean_vals[1] / count
+                mb = mean_vals[2] / count
+                print '%i: r:%.3f g:%.3f b:%.3f (unav: %i) (fail: %i)' % (
+                    count, mr, mg, mb, len(not_avail), len(fail))
+        else:
+            not_avail.add(im)
+    except:
+        fail.add(im)
 
 mr = mean_vals[0] / count
 mg = mean_vals[1] / count
