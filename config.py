a38751404dacd262805c614409d53c3d1b990713
Nick <dufour@neon-lab.com>
2016-06-24 15:07:19 -0700

    m

--- config.py
+++ config.py
@@ -50,14 +50,8 @@ LAPLACE_SMOOTHING_C = 0.05
 # Whether or not to restore the logits.
 restore_logits = True
 
-# the initial standard deviation (for initialization)
-INIT_STD = 1e-7
-
 # restore the pretrained model from this location
 pretrained_model_checkpoint_path = ''  # '/data/aquila_snaps/model.ckpt-20000'
-
-# the initial learning rate
-initial_learning_rate = 0.001
 
 # epochs after which learning rate decays
 num_epochs_per_decay = 0.05  # 1  # within-epoch decay
@@ -67,6 +61,15 @@ learning_rate_decay_factor = 0.975
 
 # whether or not to perform batch normalization
 PERFORM_BATCHNORM = False  # If not, the model diverges immediately
+
+if PERFORM_BATCHNORM:
+    INIT_STD = 0.001
+    initial_learning_rate = 0.1
+else:
+    # the initial standard deviation (for initialization)
+    INIT_STD = 1e-7
+    # the initial learning rate
+    initial_learning_rate = 0.001
 
 # NOTE: Batch size should have the same parity as the
 # average pair group.
