dd05e5c7dd741e9ea0ab6f34b8d7d904db3b629c
Nick Dufour <ndufour@eunoia.local>
2016-06-23 21:10:17 -0700

    ,

--- aquila_train.py
+++ aquila_train.py
@@ -203,7 +203,7 @@ def train(inp_mgr, test_mgr, ex_per_epoch):
     avg_loss_op = tf.reduce_mean(tf.pack(tow_loss_ops))
     avg_acc_op = tf.reduce_mean(tf.pack(tow_acc_ops))
     test_avg_acc_op = tf.reduce_mean(tf.pack(test_tow_acc_ops))
-    test_acc_avg = tf.train.ExponentialMovingAverage(0.995, name='avg_test_acc')
+    test_acc_avg = tf.train.ExponentialMovingAverage(0.95, name='avg_test_acc')
     loss_averages_op = test_acc_avg.apply([test_avg_acc_op])
     summaries.append(tf.scalar_summary('validation/accuracy_smoothed', test_acc_avg.average(test_avg_acc_op)))
     # We must calculate the mean of each gradient. Note that this is the
